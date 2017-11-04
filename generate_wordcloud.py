f4998fd9a680c94a052060d4300282bc0e19563f
Albert Wilson <avw5331@gmail.com>
2017-11-04 18:55:45 -0400

    Add files via upload

---
+++ generate_wordcloud.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+Generate a square wordcloud from a text file containing professor reviews.
+"""
+
+from os import path
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+
+def generate_wordcloud():
+    d = path.dirname(__file__)
+
+    # Read the whole text.
+    text = open(path.join(d, 'review.txt')).read()
+    # Generate a word cloud image
+    wordcloud = WordCloud().generate(text)
+
+    # Display the generated image:
+    # the matplotlib way:
+    # plt.imshow(wordcloud, interpolation='bilinear')
+    # plt.axis("off")
+
+    # lower max_font_size
+    wordcloud = WordCloud(max_font_size=40).generate(text)
+    plt.figure()
+    # plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    # plt.show()
+
+    # The pil way (if you don't have matplotlib)
+    image = wordcloud.to_image()
+    return image
+    # image.show()
+
+generate_wordcloud()
