9a00304e05d1e71cee6c440f77cf95cb19c61cd2
Sahil Mishra <sahil.mishra485@gmail.com>
2017-11-05 12:17:11 -0500

    Add files via upload

--- applicationapi.py
+++ applicationapi.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+import watson_developer_cloud.natural_language_understanding.features.v1 as Features
+import matplotlib.pyplot as plt
 
 application = Flask(__name__)
 CORS(application)
@@ -52,6 +56,138 @@ def generate_wordcloud():
 		return jsonify({
 			'response_code': 'error'
 			})
+
+def create_pie_chart(emotions_dict):
+    labels = []
+    sizes = []
+    for k,v in emotions_dict.iteritems():
+        labels.append(k)
+        sizes.append(v)
+    explode = (0,0,0,0)
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=None, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+    ax1.axis('equal')
+    fig1.savefig("piechart.png")
+    # plt.show()
+
+@application.route('/classifyText')
+def classifyText():
+
+	try:
+
+		print "classifyText() is called."
+
+		filename = request.args.get('filename')
+		textfilename = filename + '.txt'
+		print(filename)
+		d = path.dirname(__file__)
+
+		natural_language_understanding = NaturalLanguageUnderstandingV1(
+	  	username="0cd3961f-df67-403b-8c6f-0921bc01c569",
+	  	password="wQQjKUkjp2Kp",
+	  	version="2017-02-27")
+
+		fp = open(textfilename, "r")
+		text = fp.read()
+
+		response = natural_language_understanding.analyze(
+		  text=text,
+		  features=[
+		    Features.Entities(
+		      emotion=True,
+		      sentiment=True,
+		      limit=3
+		    ),
+		    Features.Keywords(
+		      emotion=True,
+		      sentiment=True,
+		      limit=3
+		    )
+		  ]
+		)
+		a = response['keywords'][0]['text'] + " with a " + response['keywords'][0]['sentiment']['label'] + " sentiment."
+		b = response['keywords'][1]['text'] + " with a " + response['keywords'][1]['sentiment']['label'] + " sentiment."
+		c = response['keywords'][2]['text'] + " with a " + response['keywords'][2]['sentiment']['label'] + " sentiment."
+		# #
+		final_string = a + b + c
+		# final_string = "Hello"
+		# print final_string
+		# print final_string
+		# return jsonify(result = str(final_string))
+		# return jsonify(result = str(final_string))
+		emotions = {'anger', 'joy', 'sadness', 'fear', 'disgust'}
+		emotions_dict = {}
+
+		for emotion in emotions:
+		    emotions_dict[emotion] = 0
+		    for i in range(3):
+		        emotions_dict[emotion] += response['keywords'][i]['emotion'][emotion]
+
+		norm = 0
+		for emotion in emotions_dict.keys():
+		    norm += emotions_dict[emotion]
+
+		for emotion in emotions_dict.keys():
+		    emotions_dict[emotion] = emotions_dict[emotion]/norm
+		# print emotions_dict
+
+		create_pie_chart(emotions_dict)
+
+		return jsonify({
+			'classified_text': final_string
+			})
+
+	except:
+		return jsonify({
+			'response_code': 'error'
+			})
+
+
+
+
+	# try:
+	# 	filename = request.args.get('filename')
+	# 	textfilename = filename + '.txt'
+	# 	print(filename)
+	# 	d = path.dirname(__file__)
+	#
+	# 	natural_language_understanding = NaturalLanguageUnderstandingV1(
+	# 	  username="0cd3961f-df67-403b-8c6f-0921bc01c569",
+	# 	  password="wQQjKUkjp2Kp",
+	# 	  version="2017-02-27")
+	#
+	# 	fp = open(textfilename, "r")
+	# 	text = fp.read()
+	#
+	# 	response = natural_language_understanding.analyze(
+	# 	  text=text,
+	# 	  features=[
+	# 	    Features.Entities(
+	# 	      emotion=True,
+	# 	      sentiment=True,
+	# 	      limit=2
+	# 	    ),
+	# 	    Features.Keywords(
+	# 	      emotion=True,
+	# 	      sentiment=True,
+	# 	      limit=2
+	# 	    )
+	# 	  ]
+	# 	)
+	# 	json_response = response
+	# 	print json_response.keys()
+	# 	final_string = json.dumps(response)
+	# 	# return jsonify(result = str(final_string))
+	# 	# return jsonify(result = str(final_string))
+	# 	return jsonify({
+	# 		'classified_text': final_string
+	# 		})
+	#
+	# except:
+	# 	return jsonify({
+	# 		'response_code': 'error'
+	# 		})
 
 
 
