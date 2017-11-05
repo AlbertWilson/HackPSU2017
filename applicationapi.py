6d9380985c0f32e242bec53625429a5707efbe6f
Sahil Mishra <sahil.mishra485@gmail.com>
2017-11-05 01:32:19 -0500

    Add files via upload

--- applicationapi.py
+++ applicationapi.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+import watson_developer_cloud.natural_language_understanding.features.v1 as Features
 
 application = Flask(__name__)
 CORS(application)
@@ -52,6 +55,101 @@ def generate_wordcloud():
 		return jsonify({
 			'response_code': 'error'
 			})
+
+@application.route('/classifyText')
+def classifyText():
+
+	try:
+
+		print "classifyText() is called."
+
+		filename = request.args.get('filename')
+		textfilename = filename + '.txt'
+		print(filename)
+		d = path.dirname(__file__)
+
+		natural_language_understanding = NaturalLanguageUnderstandingV1(
+	  	username="0cd3961f-df67-403b-8c6f-0921bc01c569",
+	  	password="wQQjKUkjp2Kp",
+	  	version="2017-02-27")
+
+		fp = open(textfilename, "r")
+		text = fp.read()
+
+		response = natural_language_understanding.analyze(
+		  text=text,
+		  features=[
+		    Features.Entities(
+		      emotion=True,
+		      sentiment=True,
+		      limit=2
+		    ),
+		    Features.Keywords(
+		      emotion=True,
+		      sentiment=True,
+		      limit=2
+		    )
+		  ]
+		)
+		json_response = response
+		print json_response
+		final_string = json.dumps(response)
+		# return jsonify(result = str(final_string))
+		# return jsonify(result = str(final_string))
+		return jsonify({
+			'classified_text': final_string
+			})
+
+	except:
+		return jsonify({
+			'response_code': 'error'
+			})
+
+
+
+
+	# try:
+	# 	filename = request.args.get('filename')
+	# 	textfilename = filename + '.txt'
+	# 	print(filename)
+	# 	d = path.dirname(__file__)
+	#
+	# 	natural_language_understanding = NaturalLanguageUnderstandingV1(
+	# 	  username="0cd3961f-df67-403b-8c6f-0921bc01c569",
+	# 	  password="wQQjKUkjp2Kp",
+	# 	  version="2017-02-27")
+	#
+	# 	fp = open(textfilename, "r")
+	# 	text = fp.read()
+	#
+	# 	response = natural_language_understanding.analyze(
+	# 	  text=text,
+	# 	  features=[
+	# 	    Features.Entities(
+	# 	      emotion=True,
+	# 	      sentiment=True,
+	# 	      limit=2
+	# 	    ),
+	# 	    Features.Keywords(
+	# 	      emotion=True,
+	# 	      sentiment=True,
+	# 	      limit=2
+	# 	    )
+	# 	  ]
+	# 	)
+	# 	json_response = response
+	# 	print json_response.keys()
+	# 	final_string = json.dumps(response)
+	# 	# return jsonify(result = str(final_string))
+	# 	# return jsonify(result = str(final_string))
+	# 	return jsonify({
+	# 		'classified_text': final_string
+	# 		})
+	#
+	# except:
+	# 	return jsonify({
+	# 		'response_code': 'error'
+	# 		})
 
 
 
